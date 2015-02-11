a2bf393817518d427710d62ca919273880c1c278
Julien Leseine <leseine.julien.logiciel@gmail.com>
2015-02-11 23:32:16 +0100

    Mise a jour fin de jeu et donjon
    - deux prefabs de fin de jeu (canvas avec redirection vers main scene)
    - une prefab de donjon avec lumières

---
+++ src/unityProject/Assets/ReloadGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReloadGame : MonoBehaviour {
+
+    void LoadMenu()
+    {
+        Application.LoadLevel("MainScene"); // TODO : donner le nom de la scene no1
+    }
+
+}
